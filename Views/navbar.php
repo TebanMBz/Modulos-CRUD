a8c9798c7240f36601c2a02267e59e380ea58374
JUAN PABLO MUÑOZ BENITEZ <esteban0605benitez@gmail.com>
2022-09-14 13:14:15 -0500

    Esteban

---
+++ Views/navbar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+    <link rel="stylesheet" href="Assets/Bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">TuristiK</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Usuarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Paquetes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Pedidos</a>
+                    </li>
+                </ul>
+                <button class="btn btn-outline-primary" type="submit">Cerrar Sesión</button>
+            </div>
+        </div>
+    </nav>
+    <script src="Assets/Bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+    <link rel="stylesheet" href="Views/Assets/Bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
+        <div class="container w-50">
+            <!--FORM CON: action="Controller/access.php"-->
+        </div>
+    </div>
+
+    <script src="Views/Assets/Bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
