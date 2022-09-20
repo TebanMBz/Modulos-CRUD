51dd6d1d4c8647dbeada415921e8510d920bfe3a
TebanMBz <109961546+TebanMBz@users.noreply.github.com>
2022-09-20 15:54:21 -0500

    Update eliminarAbono.php

--- Controller/Pedidos/Abonos/eliminarAbono.php
+++ Controller/Pedidos/Abonos/eliminarAbono.php
@@ -9,8 +9,4 @@ $consulta = $connection->prepare("DELETE FROM abonos WHERE id_abono=?");
 $resultado = $consulta->execute([$id_abono]);
 
 if($resultado){
-    echo "<h2>Registro eliminado</h2>";
-}else{
-    echo "<h2>Error al eliminar</h2>";
-}
-echo "<a href='listarAbonos.php'>Regresar</a>";
+    header("Location:listarAbonos.php");
