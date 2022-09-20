6948141258c3085607d5e253742dffc8efa2c6c7
TebanMBz <109961546+TebanMBz@users.noreply.github.com>
2022-09-20 15:53:12 -0500

    Update agregarAbono.php

--- Controller/Pedidos/Abonos/agregarAbono.php
+++ Controller/Pedidos/Abonos/agregarAbono.php
@@ -26,7 +26,7 @@ if (isset($_POST['submit'])) {
             'estado' => $estado
         ]
     );
-    if ($resultado) {
+    if ($abonos) {
         header("Location:listarBeneficiarios.php");
     }
 }
